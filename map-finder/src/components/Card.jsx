c89f9576cddbcdfb8fdc48a7d531646fe58d6422
Ben Hensor <benhensor@gmail.com>
2023-05-05 11:31:23 +0100

    card display and card components added and styled

---
+++ map-finder/src/components/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import canadaFlag from '../assets/flag-canada.webp';
+
+const Card = () => {
+  return (
+    <div className="card-container">
+    <div className="country-flag-container">
+      <img className="country-flag" src={canadaFlag} alt="" />
+    </div>
+      <div className="country-details-container">
+        {/* <img className="country-arms" src="../logo.svg" alt="" /> */}
+        <h3 className="country-title">Canada</h3>
+        <div className="country-details">
+          <p>Population:{}</p>
+          <p>Region:{}</p>
+          <p>Capital:{}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ map-finder/src/components/CardDisplay.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+const CardDisplay = () => {
+  return (
+    <section className="card-display-container">
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    </section>
+  )
+}
+
+export default CardDisplay;
