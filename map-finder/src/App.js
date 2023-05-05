6cd635dae77a57468d5d3b0fe3a0a6c2fb8b048f
Ben Hensor <benhensor@gmail.com>
2023-05-05 10:04:13 +0100

    prelimonary planning written

---
+++ map-finder/src/App.js
@@ -0,0 +1,57 @@
+// #### Things to include in your project today
+// -   Create a component tree diagram
+//     -   Should show what components have state (if any) & props (if any).
+// -   Fetching from API & Filter the data.
+// -   Recreate the design given and maybe make it better (if time permits).
+// -   Some tests
+
+// #### Bonus tasks
+// -   Explore localstorage to keep data persisting.
+// -   Make it responsive.
+// -   Include dark/light mode.
+// -   Explore accessibility.
+
+
+
+// Use API to retrieve data on countries
+// Data to include:
+// - Name
+// - Population
+// - region 
+// - capital city 
+// - coat of arms
+
+// make a landing page searchable by a variety of categories
+// populate the page with 'card' components to display country specific information
+// include links to maps 
+// results to be sortable by a range of criteria such as population etc.
+
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
